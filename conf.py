2ef88b11fea62196bec3ff9b05c6c62e264676c5
Francois 'ftiff' Levaux-Tiffreau <fti@me.com>
2017-02-28 22:20:37 +0100

    test

--- conf.py
+++ conf.py
@@ -27,6 +27,7 @@ html_context = {
 
 # app setup hook
 def setup(app):
+    app.add_stylesheet("extra.css")
     app.add_config_value('recommonmark_config', {
             'url_resolver': lambda url: github_doc_root + url,
             'auto_toc_tree_section': 'Contents',
