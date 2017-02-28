a4032ea90ba909eb9c9163e040b253a597fd5a2f
Francois 'ftiff' Levaux-Tiffreau <fti@me.com>
2017-02-28 13:15:42 +0100

    second conf.py

--- conf.py
+++ conf.py
@@ -1,7 +1,7 @@
 html_context = {
     "display_github": True, # Integrate GitHub
-    "github_user": "ftiff", # Username
-    "github_repo": "Shufflepuck", # Repo name
+    "github_user": "Shufflepuck", # Username
+    "github_repo": "MacAdminsDoc", # Repo name
     "github_version": "master", # Version
     "conf_py_path": "/", # Path in the checkout to the docs root
 }
