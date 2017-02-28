b74678229fb23aacc9a01bd90986f6e1f57dac91
Francois 'ftiff' Levaux-Tiffreau <fti@me.com>
2017-02-28 13:54:37 +0100

    small

--- conf.py
+++ conf.py
@@ -6,4 +6,12 @@ source_parsers = {
 
 source_suffix = ['.rst', '.md']
 
+html_context = {
+    "display_github": True, # Integrate GitHub
+    "github_user": "Shufflepuck", # Username
+    "github_repo": "MacAdminsDoc", # Repo name
+    "github_version": "master", # Version
+    "conf_py_path": "/", # Path in the checkout to the docs root
+}
+
 
